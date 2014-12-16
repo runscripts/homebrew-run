4329e2c91ba9b81d949420574ca64de513bbfbfa
tobe <tobeg3oogle@gmail.com>
2014-12-16 23:56:34 +0800

    Fix name in run.rb

--- run.rb
+++ run.rb
@@ -1,15 +1,17 @@
 require "formula"
 
-class Runscripts < Formula
+class Run < Formula
   homepage "http://runscripts.org"
   url "https://github.com/runscripts/run.git", :tag => "0.2.0"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build"
+    system "echo", "it works"
+    ENV["GOPATH"] = buildpath
+    system "go", "build", "-o", "run"
     bin.install "run"
-    etc.install "run.conf"
+    #etc.install "run.conf"
   end
 
   test do
